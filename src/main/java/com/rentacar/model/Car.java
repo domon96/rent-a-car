0c7fb544aca3eb3e97eaf1e41028ef92921d2b7a
Dominik <dmazur.dmazur@gmail.com>
2020-12-11 16:00:50 +0100

    added car entity

---
+++ src/main/java/com/rentacar/model/Car.java
@@ -0,0 +1,44 @@
+package com.rentacar.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.awt.*;
+
+@Entity
+@Table
+@ToString
+@EqualsAndHashCode
+@Getter
+@Builder
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String model;
+
+    @Column
+    private CarBodyType type;
+
+    @Column
+    private int productionYear;
+
+    @Column
+    private Color color;
+
+    @Column
+    private int mileage;
+
+    @Column
+    private CarStatus status;
+
+    @Column
+    private double oneDayPrice;
+}
