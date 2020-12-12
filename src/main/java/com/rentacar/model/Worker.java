db61fd0ad872a8d9392ce29b50ca93d6553fdefb
Dominik <dmazur.dmazur@gmail.com>
2020-12-12 10:05:16 +0100

    added worker entity

---
+++ src/main/java/com/rentacar/model/Worker.java
@@ -0,0 +1,31 @@
+package com.rentacar.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private JobTitle jobTitle;
+
+    @Column
+    @OneToOne
+    private Department department;
+}
