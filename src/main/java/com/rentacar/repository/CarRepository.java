6060e9db54502a9ac3a8b64c71cdbdd8e1d51008
Dominik <dmazur.dmazur@gmail.com>
2020-12-12 09:51:59 +0100

    added annotation @Repository

--- src/main/java/com/rentacar/repository/CarRepository.java
+++ src/main/java/com/rentacar/repository/CarRepository.java
@@ -2,8 +2,10 @@ package com.rentacar.repository;
 
 import com.rentacar.model.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called clientRepository
 
+@Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
--- src/main/java/com/rentacar/repository/ClientRepository.java
+++ src/main/java/com/rentacar/repository/ClientRepository.java
@@ -2,8 +2,10 @@ package com.rentacar.repository;
 
 import com.rentacar.model.Client;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called clientRepository
 
+@Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
 }
