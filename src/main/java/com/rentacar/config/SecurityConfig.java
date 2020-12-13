ce2f7fed50a8b0975b08d2f677068167d2f6d514
Dominik <dmazur.dmazur@gmail.com>
2020-12-13 14:03:52 +0100

    not working - roles

--- src/main/java/com/rentacar/config/SecurityConfig.java
+++ src/main/java/com/rentacar/config/SecurityConfig.java
@@ -6,53 +6,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .withDefaultSchema()
-//                .withUser(User.withUsername())
-//    }
+    @Autowired
+    private DataSource dataSource;
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user1")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin1")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("SELECT u.login, u.password, 1 FROM user u WHERE u.login=?")
+                .authoritiesByUsernameQuery("SELECT u.login, r.role, 1 " +
+                        "FROM user u " +
+                        "INNER JOIN user_role ur ON ur.user_id = u.id " +
+                        "INNER JOIN role r ON r.role_id = ur.role_id " +
+                        "WHERE u.login=?")
+                .dataSource(dataSource);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // NOT WORKING - roles
+
         http.authorizeRequests()
-                .antMatchers("/clients/**").permitAll()
-                .anyRequest().hasRole("ADMIN")
+                .antMatchers("/clients/**").hasRole("USER")
+                .antMatchers("/rentals/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll();
+
+        http.csrf().disable().headers().frameOptions().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
