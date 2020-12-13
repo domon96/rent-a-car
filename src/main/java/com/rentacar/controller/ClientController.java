4fdb8f427c507fd2860d9e0c9649c8b66cfc77ea
Dominik <dmazur.dmazur@gmail.com>
2020-12-13 14:00:22 +0100

    added basic client controller

---
+++ src/main/java/com/rentacar/controller/ClientController.java
@@ -0,0 +1,13 @@
+package com.rentacar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+
+    @GetMapping(path = "/")
+    public String main() {
+        return "home";
+    }
+}
