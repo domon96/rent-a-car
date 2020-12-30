63d2a2df0f826a1a521b36b4154d62f73e3c0c68
Dominik <dmazur.dmazur@gmail.com>
2020-12-30 18:25:19 +0100

    renamed

--- src/main/java/com/rentacar/repository/RoleRepository.java
+++ src/main/java/com/rentacar/repository/RoleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
+public interface RoleRepository extends CrudRepository<UserRole, Long> {
 
     UserRole getByRole(RoleType roleType);
 }
--- src/main/java/com/rentacar/repository/UserRepository.java
+++ src/main/java/com/rentacar/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.rentacar.repository;
 
-import com.rentacar.model.Client;
+import com.rentacar.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called clientRepository
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
