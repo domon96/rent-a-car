6edb858fb82cb673a2c2783f9a71e141c01fb9fd
Dominik <dmazur.dmazur@gmail.com>
2020-12-30 18:24:32 +0100

    added user DTO

---
+++ src/main/java/com/rentacar/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.rentacar.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String login;
+    private String password;
+    private String confirmedPassword;
+}
